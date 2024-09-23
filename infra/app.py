3b763df385897eaafbee4e1b5272128ee526237c
Zuri Hunter <zuri@blackcape.io>
2024-09-22 23:26:14 -0400

    add: Add in rest of files

--- infra/app.py
+++ infra/app.py
@@ -2,27 +2,33 @@
 import os
 
 import aws_cdk as cdk
+from dotenv import load_dotenv
 
-from infra.platform import ThebisonplatformStack
+from platforms.tools.registry.tbp_shinyproxyecr import ShinyProxyECRStack
 
+from platforms.directory.auth.tbp_cognito import ShinyProxyCognitoStack
+
+load_dotenv('../.env')
+account_id = os.getenv('AWS_ID')
 
 app = cdk.App()
-ThebisonplatformStack(app, "ThebisonplatformStack",
-    # If you don't specify 'env', this stack will be environment-agnostic.
-    # Account/Region-dependent features and context lookups will not work,
-    # but a single synthesized template can be deployed anywhere.
 
-    # Uncomment the next line to specialize this stack for the AWS Account
-    # and Region that are implied by the current CLI configuration.
-
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
-
-    # Uncomment the next line if you know exactly what Account and Region you
-    # want to deploy the stack to. */
-
-    #env=cdk.Environment(account='123456789012', region='us-east-1'),
-
-    # For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html
+ShinyProxyECRStack(
+    app,
+    "TBPShinyProxyECR",
+    env=cdk.Environment(
+        account=account_id,
+        region='us-east-1'
     )
+)
+
+ShinyProxyCognitoStack(
+    app,
+    'TBPShinyProxyCognito',
+    env=cdk.Environment(
+        account=account_id,
+        region='us-east-1'
+    )
+)
 
 app.synth()
